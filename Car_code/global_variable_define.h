1682eb90e19fcb02817b3781876c5f2cb0a78048
lanouyu <lanouyu@gmail.com>
2016-05-15 20:18:16 +0800

    Create global_variable_define.h

---
+++ Car_code/global_variable_define.h
@@ -0,0 +1,6 @@
+#ifndef __GLOBAL_VARIABLE_DEFINE_H__
+#define __GLOBAL_VARIABLE_DEFINE_H__
+//全局变量定义global variable，该文件放在主程序中，其他子程序中放入global_variable_declare.h去申明。
+
+volatile unsigned int rev_data;
+#endif
